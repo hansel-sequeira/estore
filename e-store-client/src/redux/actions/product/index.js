e03f490ed84cad1c1bb5f38a4c60fe083847307c
Hansel Sequeira <sequeirahansel@gmail.com>
2022-02-19 22:33:56 +0530

    Served static product images from server to client end

--- e-store-client/src/redux/actions/product/index.js
+++ e-store-client/src/redux/actions/product/index.js
@@ -1,28 +1,6 @@
 import * as actionTypes from "./types";
 import axios from "axios";
 
-const products = [
-
-    {
-        id: 1,
-        src: require("../../../assets/img/shop/shop-1.jpg"),
-        name: "Product 1",
-        price: "$10.00"
-    },
-    {
-        id: 2,
-        src: require("../../../assets/img/shop/shop-2.jpg"),
-        name: "Product 2",
-        price: "$20.00"
-    },
-    {
-        id: 3,
-        src: require("../../../assets/img/shop/shop-3.jpg"),
-        name: "Product 3",
-        price: "$30.00"
-    }
-];
-
 export const getProductCategories = () => async (dispatch) => {
 
     let tempArray = [];
@@ -30,8 +8,6 @@ export const getProductCategories = () => async (dispatch) => {
         method: "get",
         url: "http://localhost:5000/product/api/getCategories"
     }).then((res)=>{
-        console.log("The response from API: ", res.data);
-
         let parentCategory = res.data.data.filter(item => item.parent_category_id === null)
 
         tempArray = parentCategory.map( item =>{
@@ -51,19 +27,25 @@ export const getProductCategories = () => async (dispatch) => {
     }).catch((err)=>{
         console.log("Error while retrieving product categories from the database! ", err);
     })
-
-    console.log("Temp array: ", tempArray);
     dispatch({type:actionTypes.PRODUCT_CATEGORY, categories: tempArray})
 }
 
 export const getProducts = () => async (dispatch) => {
+    let productArr = [];
     await axios({
         method: "get",
         url: "http://localhost:5000/product/api/getProducts"
     }).then((res)=>{
-        console.log(res.data);
+        productArr = res.data.data.map((item)=>{
+            return {
+                id: item.id,
+                src: `http://localhost:5000/${item.img}`,
+                name: item.name,
+                price: item.price
+            }
+        })
     }).catch((err)=>{
         console.log("Error while retrieving products from the database! ", err);
     })
-    dispatch({type:actionTypes.PRODUCT, data: products});
+    dispatch({type:actionTypes.PRODUCT, data: productArr});
 } 
--- e-store-server/index.js
+++ e-store-server/index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require("cors");
 const product = require("./Routes/product");
 
+
 app.use(cors());
+app.use("/", express.static("Uploads"));
 app.use("/product/api/", product);
 
 const PORT = 5000;
